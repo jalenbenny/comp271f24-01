d8acb4f65e7a686cd83c3904aa9b773a3fc46178
ben flowers <149430451+jalenbenny@users.noreply.github.com>
2024-12-08 15:00:39 -0600

    Update SimpleLinkedList.java

--- 13-wrapup/SimpleLinkedList.java
+++ 13-wrapup/SimpleLinkedList.java
@@ -1,77 +1,33 @@
 /**
- * A simple linked list object. Its nodes are defined as an inner class.
- * 
- * COMP 271 FINAL ASSIGNMENT
- * 
- * Review class SimpleLinkedList below. It's a linked list object, quite similar
- * to our TrainLine, only instead of TrainStations it uses Nodes. The nodes are
- * defined as a class-within-the-class -- this is called an inner class and it's
- * a useful coding practice for simple situations like this one here.
- * 
- * Your job is to complete TWO methods in the SimpleLinkedList. You may NOT
- * modify any part of the class and you may not import ANYTHING. Your code
- * should be focused only within the two methods you are asked to write.
- * 
- * For this assignment, you may NOT use arrays or arraylists or anything else
- * other than the SimpleLinkedList data structure.
- * 
- * 1) Write method findMiddle() that finds and returns the middle node of
- * a SimpleListList. For example, if the SimpleLinkedList object is:
- * 
- * A -> null ... the middle node is A
- * 
- * When the SimpleLinkedList object is:
- * 
- * A -> B --> null ... the middle node is also A
- * 
- * When the SimpleLinkedList object is:
- * 
- * A -> B --> C --> null ... the middle node is B
- * 
- * When the SimpleLinkedList object is:
- * 
- * A -> B --> C --> D --> null ... the middle node is also B
- * 
- * When the SimpleLinkedList object is:
- * 
- * A -> B --> C --> D --> E --> null ... the middle node is C, etc
- * 
- * 
- * 2) Write a method called invert that returns the inverted version of the
- * present SimpleLinkedList object. For example, if the current object is
- * 
- * A --> B --> C --> D --> E --> null
- * 
- * method invert should return the object
- * 
- * E --> D --> C --> B --> A --> null.
- * 
- * Remember, no arrays or arraylists or any other data structure except for
- * SimpleLinkedList.
- * 
+ * simple linked list implementation
+ * contains nodes and methods to manipulate the linked list structure
  */
-
 public class SimpleLinkedList {
 
     /**
-     * Inner class for Node. Node fields can be accessed directly, for simplicity of
-     * code. This is an intentional violation of the Pact.
+     * node class represents an element in the linked list
+     * each node contains data and a reference to the next node
      */
     class Node {
         String data;
         Node next;
 
-        /** Simple string representation for Node */
+        /**
+         * returns the string representation of the node
+         */
         public String toString() {
             return this.data;
-        } // method Node.toString
-    } // inner class Node
+        }
+    }
 
-    /** The only field in class SimpleLinkedList */
+    /**
+     * head represents the start of the linked list
+     */
     Node head;
 
     /**
-     * Add a new node to the linked list
+     * adds a new node with the given data to the end of the list
+     * traverses the list to find the last node and links the new node
      */
     public void add(String data) {
         Node newNode = new Node();
@@ -79,38 +35,58 @@ public class SimpleLinkedList {
         if (this.head == null) {
             this.head = newNode;
         } else {
-            // Traverse the list to find the last node
             Node current = this.head;
             while (current.next != null) {
                 current = current.next;
             }
-            // current is now the last node in the list
             current.next = newNode;
         }
-    } // method add
+    }
 
     /**
-     * Find and return the middle node of the linked list.
-     * 
-     * Remember, no arrays or arraylists or any other data structure except for
-     * SimpleLinkedList.
-     * 
+     * finds and returns the middle node of the linked list
+     * uses two pointers: slow and fast, where fast moves twice as quickly
      */
     public Node findMiddle() {
-    } // method SimpleLinkedList.findMiddle
+        if (head == null) {
+            return null;
+        }
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
 
     /**
-     * Invert a linked list.
-     * 
-     * Remember, no arrays or arraylists or any other data structure except for
-     * SimpleLinkedList.
-     * 
-     * For this method you may NOT use method SimpleLinkedList.add
+     * inverts the linked list so that the order of nodes is reversed
+     * iteratively adjusts the next pointers of the nodes to invert the list
      */
     public SimpleLinkedList invert() {
-    } // method SimpleLinkedList.invert
+        SimpleLinkedList invertedList = new SimpleLinkedList();
+        Node current = head;
+        Node prev = null;
 
-    /** String representation for the simple linked list */
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        invertedList.head = prev;
+        return invertedList;
+    }
+
+    /**
+     * returns the string representation of the linked list
+     * concatenates the data of each node in the list
+     */
     public String toString() {
         StringBuilder sb = new StringBuilder();
         if (this.head != null) {
@@ -121,42 +97,33 @@ public class SimpleLinkedList {
             }
         }
         return sb.toString();
-    } // method SimpleLinkedList.toString
+    }
 
     /**
-     * Driver/test code. This is some of the most embarassing code I've ever written
-     * but it works. Learn from it and do not write code that bad! :-)
+     * demonstrates the functionality of the linked list
+     * prints the middle node and the inverted list for testing
      */
     public static void main(String[] args) {
         SimpleLinkedList demo = new SimpleLinkedList();
-        boolean test1 = demo.findMiddle() == null;
+
+        System.out.println("Middle node when list is empty: " + demo.findMiddle());
+
         demo.add("A");
-        boolean test2 = demo.findMiddle().data.equals("A");
+        System.out.println("Middle node: " + demo.findMiddle());
+
         demo.add("B");
-        boolean test3 = demo.findMiddle().data.equals("A");
+        System.out.println("Middle node: " + demo.findMiddle());
+
         demo.add("C");
-        boolean test4 = demo.findMiddle().data.equals("B");
+        System.out.println("Middle node: " + demo.findMiddle());
+
         demo.add("D");
         demo.add("E");
-        boolean test5 = demo.findMiddle().data.equals("C");
-        boolean success = test1 && test2 && test3 && test4 && test5;
-        if (success) {
-            System.out.println("Method findMiddle works as specified.");
-        } else {
-            System.out.println("Method findMiddle not working as specified.");
-        }
-        String left = demo.toString();
-        String right = demo.invert().toString();
-        boolean test10 = left.length() == right.length();
-        for (int i = 0; i < left.length(); i++) {
-            test10 = test10
-                    && left.charAt(i) == right.charAt(right.length() - 1 - i);
-        }
-        if (test10) {
-            System.out.println("Method invert works as specified.");
-        } else {
-            System.out.println("Method invert not working as specified.");
-        }
-    } // method SimpleLinkedList.main
+        System.out.println("Middle node: " + demo.findMiddle());
 
-} // class SimpleLinkedList
+        System.out.println("Original list: " + demo);
+        System.out.println("Inverted list: " + demo.invert());
+    }
+}
+
+
